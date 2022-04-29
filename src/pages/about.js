9152b2abfafdc54fdbee42212b8c5bb7b656fc83
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-04-29 12:41:14 -0400

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+//pages are export default function component
+
+export default function About() {
+  return (
+    <main>
+      <h1>About Page</h1>
+    </main>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+//pages are export default function component
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
